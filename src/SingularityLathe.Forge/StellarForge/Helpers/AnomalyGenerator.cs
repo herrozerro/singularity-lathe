2a5e52b9bf8143ca6ea9a283fbb38c0fb1af6a6f
Josiah Bradbury <herrozerro@gmail.com>
2019-10-23 00:09:04 -0500

    Started Anomaly Generator

--- src/SingularityLathe.Forge/StellarForge/Helpers/AnomalyGenerator.cs
+++ src/SingularityLathe.Forge/StellarForge/Helpers/AnomalyGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SingularityLathe.Forge.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,22 @@ namespace SingularityLathe.Forge.StellarForge.Helpers
 {
     internal class AnomalyGenerator
     {
+        private readonly Random rnd = null;
+        private readonly MadLibService madLibService = null;
+
+        public AnomalyGenerator(Random rnd, MadLibService madLibService)
+        {
+            this.rnd = rnd;
+            this.madLibService = madLibService;
+
+            var Types = new MadLib()
+            {
+                Name = "Type"
+            };
+
+            Types.Values.Add("Magnetic");
+            Types.Values.Add("Gravametric");
+            Types.Values.Add("Radiatitive");
+        }
     }
 }
